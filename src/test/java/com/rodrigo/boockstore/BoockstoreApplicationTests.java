2c5f11576e6805465a3990897b6d711d24c6ff28
Rodrigo Goncalves <ro.goncalve@gmail.com>
2022-04-15 10:33:15 -0300

    commit inicial

---
+++ src/test/java/com/rodrigo/boockstore/BoockstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.boockstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoockstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
