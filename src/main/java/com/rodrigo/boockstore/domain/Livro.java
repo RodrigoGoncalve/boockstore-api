eec3866e4620ddeef4cd391855d9c335623603dd
Rodrigo Goncalves <ro.goncalve@gmail.com>
2022-04-18 16:09:49 -0300

    Finalizando as classes de dominio

--- src/main/java/com/rodrigo/boockstore/domain/Livro.java
+++ src/main/java/com/rodrigo/boockstore/domain/Livro.java
@@ -1,8 +1,19 @@
 package com.rodrigo.boockstore.domain;
 
-public class Livro {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Livro implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 	
-	private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)private Integer id;
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
