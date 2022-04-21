4a8a5e7e39c89afdab818c2dfea459eaaa3a4141
Rodrigo Goncalves <ro.goncalve@gmail.com>
2022-04-21 13:00:30 -0300

    findAll para categoria

--- src/main/java/com/rodrigo/boockstore/resources/CategoriaResource.java
+++ src/main/java/com/rodrigo/boockstore/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.rodrigo.boockstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodrigo.boockstore.domain.Categoria;
+import com.rodrigo.boockstore.dtos.CategoriaDTO;
 import com.rodrigo.boockstore.service.CategoriaService;
 
 @RestController
@@ -21,5 +25,17 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria>findById(@PathVariable Integer id){
 		Categoria obj = categoriaService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		
+		List<Categoria> list = categoriaService.findAll();
+		
+		List<CategoriaDTO> listDto = list.stream()
+				.map(obj -> new CategoriaDTO(obj))
+				.collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
 	}
 }
