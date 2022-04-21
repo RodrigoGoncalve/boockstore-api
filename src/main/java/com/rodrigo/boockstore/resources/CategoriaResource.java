6014a78ad5555b120ff75d9c8f6f7bc833277a93
Rodrigo Goncalves <ro.goncalve@gmail.com>
2022-04-21 14:25:52 -0300

    Criando post para categoria

--- src/main/java/com/rodrigo/boockstore/resources/CategoriaResource.java
+++ src/main/java/com/rodrigo/boockstore/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.rodrigo.boockstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodrigo.boockstore.domain.Categoria;
 import com.rodrigo.boockstore.dtos.CategoriaDTO;
@@ -37,5 +41,19 @@ public class CategoriaResource {
 				.collect(Collectors.toList());
 		
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+		
+		obj = categoriaService.create(obj);
+		
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId())
+				.toUri();
+		
+		//return ResponseEntity.created(uri).body(obj);
+		return ResponseEntity.created(uri).build();
 	}
 }
